39e31b372e07f4ec5e2a55b31a323d97f6862e29
Kesavadas <kesavadaskesh@gmail.com>
2025-09-09 12:36:19 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["s2.coinmarketcap.com"], // allow CoinMarketCap images
+  },
 };
 
 module.exports = nextConfig;
