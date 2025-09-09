556a5296887a661681bebcbfe592653aaa95ef61
Kesavadas <kesavadaskesh@gmail.com>
2025-09-09 12:00:33 +0400

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchCryptoData } from '@/lib/data';
-import CryptoClientPage from './crypto-client-page';
-
-export default async function Home() {
-  const initialData = await fetchCryptoData();
-  return <CryptoClientPage initialData={initialData} />;
-}
