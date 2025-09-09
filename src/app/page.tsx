1316f7f8564f7fa54a63c2eb95124cd8f3401a8c
Kesavadas <kesavadaskesh@gmail.com>
2025-09-09 13:04:47 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { fetchCryptoData } from '@/lib/data';
 import CryptoClientPage from './crypto-client-page';
 
+export const dynamic = 'force-dynamic'; // Add this line here
+
 export default async function Home() {
   const initialData = await fetchCryptoData();
   return <CryptoClientPage initialData={initialData} />;
